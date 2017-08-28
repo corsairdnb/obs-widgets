d38482286e3028a18e5766506df70790ed9442ad
dmitry <dmitry.rychkov@team.wrike.com>
2017-08-28 16:10:51 +0300

    empty intro

--- intro/intro.js
+++ intro/intro.js
@@ -1,3 +1,17 @@
 $(function () {
-  $('.program-name').load('program.txt');
+
+  $.get('program.txt', function(text) {
+    if (text) {
+      $('.program-name').text(text);
+      $('.program-name').show();
+      $('.logo-1').show();
+      $('.logo-2').hide();
+    } else {
+      $('.program-name').hide();
+      $('.program-name').text('');
+      $('.logo-2').show();
+      $('.logo-1').hide();
+    }
+  });
+
 });
