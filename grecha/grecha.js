7a570457b79b48f02490c2cea2ca59283a30e49e
Dmitry Rychkov <dmitry.rychkov@team.wrike.com>
2018-06-20 21:57:04 +0300

    grecha

---
+++ grecha/grecha.js
@@ -0,0 +1,23 @@
+$(function () {
+
+    // $.get('timetable.txt', function (text) {
+    //     if (!text) return;
+    //
+    //     text.split('\n').forEach(function (item) {
+    //         item = item.split(/^(.*?)\s/)
+    //         timetable.append('<div class="timetable__item"><b>' + item[1] + '</b> ' + item[2] + '</div>')
+    //     });
+    //
+    //     setInterval(function(){
+    //         $('.timetable__item').each(function (i, e) {
+    //             setTimeout(function (el) {
+    //                 $(el).addClass('animated pulse');
+    //             }, 1000 * (i + 1), e)
+    //             setTimeout(function (el) {
+    //                 $(el).removeClass('animated pulse');
+    //             }, 1000 * (i + 1) + 1000, e)
+    //         });
+    //     }, 20000);
+    // });
+
+});
