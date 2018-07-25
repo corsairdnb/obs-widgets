d1bbe0c2d6112405adb8eb2d3b4aff500ecbcb7d
Dmitry Rychkov <dmitry.rychkov@team.wrike.com>
2018-07-25 17:01:26 +0300

    donation

--- donation/index.js
+++ donation/index.js
@@ -1,79 +1,133 @@
 $(function () {
 
+    var queue = [];
+    var isBannerRun = false;
+
+    var CLASS_NAME = 'donation__name';
+    var CLASS_AMOUNT = 'donation__amount';
+    var CLASS_MESSAGE = 'donation__message';
+    var CLASS_HEADER = 'donation__header';
+    var CLASS_LAYER_1 = 'layer-1';
+    var CLASS_LAYER_2 = 'layer-2';
+
     var DELAY_1 = 100;
     var DELAY_2 = 200;
     var DELAY_3 = 1400;
     var DELAY_4 = 1500;
     var DELAY_5 = 1600;
-
     var DELAY_PAUSE = 8000;
+    var LOOP_GAP = 1000;
+
+    var ANIMATION_LENGTH = DELAY_1 + DELAY_2 + DELAY_3 + DELAY_4 + DELAY_5 + DELAY_PAUSE;
+    var DONATE_LOOP_LENGTH = ANIMATION_LENGTH + LOOP_GAP;
+    var BANNER_LOOP_LENGTH = 1000 * 60 * 3; // 3 minutes
+
+    main();
+
+    function main() {
+        initSocket();
+
+        setInterval(function () {
+            if (!isBannerRun) {
+                showDonation();
+            }
+        }, DONATE_LOOP_LENGTH);
+
+        setInterval(function () {
+            showBanner();
+        }, BANNER_LOOP_LENGTH);
+    }
+
+    function showBanner() {
+        isBannerRun = true;
+    }
+
+    function showDonation() {
+        var text = queue.shift();
+        var donation;
+        if (typeof text !== 'undefined' && text) {
+            donation = JSON.parse(text);
+        }
+
+        if (typeof donation !== 'undefined' && donation) {
+            run(
+                donation['username'],
+                donation['amount_formatted'] + ' ' + donation['currency'],
+                donation['message']
+            );
+        }
+    }
 
     function animateIn(name, delay) {
         setTimeout(function () {
             $('.' + name).addClass('bounceInLeft');
         }, delay);
     }
+
     function animateOut(name, delay) {
         setTimeout(function () {
             $('.' + name).removeClass('bounceInLeft');
         }, delay);
     }
+
     function run(name, amount, message) {
-        $('.donation__name').text(name);
-        $('.donation__amount').text(amount);
-        $('.donation__message').text(message);
+        $('.' + CLASS_NAME).text(name);
+        $('.' + CLASS_AMOUNT).text(amount);
+        $('.' + CLASS_MESSAGE).text(message);
 
-        animateIn('layer-1', DELAY_1);
-        animateIn('layer-2', DELAY_2);
-        animateIn('donation__header', DELAY_3);
-        animateIn('donation__name', DELAY_4);
-        animateIn('donation__message', DELAY_5);
+        animateIn(CLASS_LAYER_1, DELAY_1);
+        animateIn(CLASS_LAYER_2, DELAY_2);
+        animateIn(CLASS_HEADER, DELAY_3);
+        animateIn(CLASS_NAME, DELAY_4);
+        animateIn(CLASS_MESSAGE, DELAY_5);
 
-        animateOut('donation__message',  DELAY_PAUSE + DELAY_3);
-        animateOut('donation__name',  DELAY_PAUSE + DELAY_4);
-        animateOut('donation__header',  DELAY_PAUSE + DELAY_5);
-        animateOut('layer-1',  DELAY_PAUSE + DELAY_5 + DELAY_1);
-        animateOut('layer-2',  DELAY_PAUSE + DELAY_5 + DELAY_2);
+        animateOut(CLASS_MESSAGE, DELAY_PAUSE + DELAY_3);
+        animateOut(CLASS_NAME, DELAY_PAUSE + DELAY_4);
+        animateOut(CLASS_HEADER, DELAY_PAUSE + DELAY_5);
+        animateOut(CLASS_LAYER_1, DELAY_PAUSE + DELAY_5 + DELAY_1);
+        animateOut(CLASS_LAYER_2, DELAY_PAUSE + DELAY_5 + DELAY_2);
     }
 
-    run('name', 'text', 10000);
+    function initSocket() {
+        var socket = io('socket.donationalerts.ru:3001', {
+            reconnection: true,
+            reconnectionDelayMax: 5000,
+            reconnectionDelay: 1000
+        });
+        var token = 'e4UuFIW6uHFm115XZEcf';
 
-    var socket = io('socket.donationalerts.ru:3001', {
-        reconnection: true,
-        reconnectionDelayMax: 5000,
-        reconnectionDelay: 1000
-    });
-    var token = 'e4UuFIW6uHFm115XZEcf';
+        socket.on('connect', function (msg) {
+            console.info('%c WS: connected', 'color: green');
+            socket.emit('add-user', {token: token, type: 'alert_widget'});
+        });
 
-    socket.on('connect', function (msg) {
-        console.info('%c WS: connected', 'color: green');
-        socket.emit('add-user', {token: token, type: 'alert_widget'});
-    });
+        socket.on('connect_error', function (msg) {
+            console.error('WS: connection_error');
+        });
 
-    socket.on('connect_error', function (msg) {
-        console.error('WS: connection_error');
-    });
+        socket.on('connect_timeout', function (msg) {
+            console.info('WS: connection_timeout');
+        });
 
-    socket.on('connect_timeout', function (msg) {
-        console.info('WS: connection_timeout');
-    });
+        socket.on('reconnect', function (msg) {
+            console.info('WS: reconnected');
+        });
 
-    socket.on('reconnect', function (msg) {
-        console.info('WS: reconnected');
-    });
+        socket.on('donation', function (msg) {
+            enqueue(msg);
+        });
+    }
 
-    socket.on('donation', function (msg) {
-        var donation = $.parseJSON(msg);
-
-        run(
-            donation.username,
-            donation.amount_formatted + ' ' + donation.currency,
-            donation.message
-        );
-    });
+    function enqueue(element) {
+        var found = queue.some(function (value) {
+            return value.toString() === element.toString();
+        });
+        if (!found) {
+            queue.push(element);
+        }
+    }
 
 });
-
 
 
 // socket.on('update-alert_widget', function (msg) {
